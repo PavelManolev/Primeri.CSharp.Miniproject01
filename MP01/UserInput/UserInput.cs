a363726de06e54c9355304474ec82bf7d3aaa87d
Pavel Manolev <PavelManolev@abv.bg>
2016-01-10 15:43:19 +0200

    Modul6.уру¾к 4,Библиотека за основното меню

---
+++ MP01/UserInput/UserInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UserInput
+{
+	public class UInput
+	{
+		public UInput ()
+		{
+		}
+
+		public void getUserCommands ()
+		{
+			string _command = "";
+			do 
+			{
+				//Взимане на команда
+				Console.Write ("$ ");
+				_command = Console.ReadLine ();
+
+				//Проверка за налични команди
+				if ( _command.ToLower().Contains ("команда1")) Console.WriteLine (" Стартирана е Команда 1\n");
+				if ( _command.ToLower().Contains ("команда2")) Console.WriteLine (" Стартирана е Команда 2\n");
+				if ( _command.ToLower().Contains ("команда3")) Console.WriteLine (" Стартирана е Команда 3\n");
+
+			} while (_command.ToLower() != "изход");
+		}
+
+		public void justTesting ()
+		{
+			Console.WriteLine (" Стартиране на UserInput.UInput.justTesting ");
+		}
+
+	}
+}
+
